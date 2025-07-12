72ba7d0fec048c5e57586a17d98477800aaec2d7
patrick-burasan24 <cristi.burasan@gmail.com>
2025-07-12 14:49:54 +0300

    add tren_japonez.cpp

---
+++ tren_japonez.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    n vagoane
+    m marfa
+    I = marfa / vagoane
+*/
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int I = m / n;
+    cout << I << '\n';
+    return 0;
+}
